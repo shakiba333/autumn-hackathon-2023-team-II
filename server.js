c9d33b9a0332dc833ccfa9653b8c6bf4324489b1
Damola Olusakin <olusakinadedamola@gmail.com>
2023-10-24 12:59:33 -0400

    added utils folder for API calls

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
 // Set up routes and middleware here
 
 const PORT = process.env.PORT || 3000;
