9fba33a7f6551520f09c1fdddd8e1a9e5e9f8b0d
jabbacalvin56 <jabbacalvin@gmail.com>
2023-10-26 16:00:31 -0500

    combine login logic within user creation function

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -4,16 +4,16 @@ const Group = require("../models/group");
 
 module.exports = {
   create,
-  login,
+  // login,
 };
 
 async function create(req, res) {
   try {
-    console.log(req.body);
     const user = await User.findOne({ googleId: req.body.googleId });
 
     if (user) {
-      res.status(500).json({ error: "Email already in use." });
+      res.status(200).json(user);
+      return;
     }
 
     const newGroup = await Group.create(req.body);
@@ -36,14 +36,14 @@ async function create(req, res) {
   }
 }
 
-async function login(req, res) {
-  try {
-    const user = await User.findOne({ googleId: req.body.id });
+// async function login(req, res) {
+//   try {
+//     const user = await User.findOne({ googleId: req.body.googleId });
 
-    if (!user) throw new Error();
+//     if (!user) throw new Error();
 
-    res.json(user);
-  } catch (err) {
-    res.status(400).json("Unable to login");
-  }
-}
+//     res.json(user);
+//   } catch (err) {
+//     res.status(400).json("Unable to login");
+//   }
+// }
