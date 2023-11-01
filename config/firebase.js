f9b6d7d664c5379542d1fc7025db067919882184
jabbacalvin56 <jabbacalvin@gmail.com>
2023-11-01 10:22:19 -0500

    remove local storage when logout

--- config/firebase.js
+++ config/firebase.js
@@ -1,23 +1,20 @@
 // import firebase from "firebase";
 // import Constants from "expo-constants";
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_PROJECTID,
-  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_APPID,
-  measurementId: process.env.REACT_APP_MEASUREMENTID,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSENGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
+const Firebase = initializeApp(firebaseConfig);
+const db = getFirestore(Firebase);
 
-
-
-  const Firebase = initializeApp(firebaseConfig);
-  const db = getFirestore(Firebase)
-
-  export { Firebase, db };
+export { Firebase, db };
